6404433cb63ec8ff8200aae1136988dff4708aa8
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-09-05 22:29:17 +0200

    feat: Init project

---
+++ pricing-service/src/test/java/pl/uncleglass/pricingservice/PricingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uncleglass.pricingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
