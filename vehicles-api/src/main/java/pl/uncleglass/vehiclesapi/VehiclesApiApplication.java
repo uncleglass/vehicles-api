b4eb95fd7f9c9ae3c98956c5e34376a7d46714db
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-09-07 10:17:14 +0200

    feat: Add Vehicles Api service

---
+++ vehicles-api/src/main/java/pl/uncleglass/vehiclesapi/VehiclesApiApplication.java
@@ -0,0 +1,13 @@
+package pl.uncleglass.vehiclesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesApiApplication.class, args);
+	}
+
+}
---
+++ vehicles-api/src/test/java/pl/uncleglass/vehiclesapi/VehiclesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uncleglass.vehiclesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
