1e753e1ab3d7f6e9512ca121219bbea8a083bc35
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-09-09 06:08:03 +0200

    feat: Implement delete() from Car Service

--- api/src/main/java/pl/uncleglass/api/service/CarService.java
+++ api/src/main/java/pl/uncleglass/api/service/CarService.java
@@ -78,16 +78,8 @@ public class CarService {
      * @param id the ID number of the car to delete
      */
     public void delete(Long id) {
-        /**
-         * TODO: Find the car by ID from the `repository` if it exists.
-         *   If it does not exist, throw a CarNotFoundException
-         */
-
-
-        /**
-         * TODO: Delete the car from the repository.
-         */
-
-
+        Car car = carRepository.findById(id)
+                .orElseThrow(CarNotFoundException::new);
+        carRepository.delete(car);
     }
 }
