0ed9358f39aa49fa68932facda65733ce942ad00
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-09-09 04:30:24 +0200

    feat: Add Boogle Maps Service and register it on the Eureka server

---
+++ boogle-maps/src/main/java/pl/uncleglass/booglemaps/BoogleMapsApplication.java
@@ -0,0 +1,15 @@
+package pl.uncleglass.booglemaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class BoogleMapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoogleMapsApplication.class, args);
+	}
+
+}
---
+++ boogle-maps/src/test/java/pl/uncleglass/booglemaps/BoogleMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uncleglass.booglemaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoogleMapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
