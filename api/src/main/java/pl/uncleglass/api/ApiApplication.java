6146c4786a0e4919bd657497ae7063e53d8e3454
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-09-12 13:24:16 +0200

    feat: Add Swagger to The Api Service

--- api/src/main/java/pl/uncleglass/api/ApiApplication.java
+++ api/src/main/java/pl/uncleglass/api/ApiApplication.java
@@ -1,7 +1,6 @@
 package pl.uncleglass.api;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
